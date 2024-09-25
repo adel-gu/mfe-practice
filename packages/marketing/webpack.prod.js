2fe7ec998b523566a10af0c359db257040d5c36f
adel <adeelguitoun@gmail.com>
2024-09-25 11:08:45 +0100

    Config production env

---
+++ packages/marketing/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const { ModuleFederationPlugin } = require('webpack').container;
+const commonConfig = require('./webpack.common');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap.js',
+      },
+      shared: {
+        react: { singleton: true },
+        'react-dom': { singleton: true },
+      },
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
